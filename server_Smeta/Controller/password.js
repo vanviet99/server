dc3d1a695bccf07ed4a872f7ff9fc582b89d82e2
turoyal <anhturoyal@gmail.com>
2023-09-10 13:37:31 +0700

    123123

--- server_Smeta/Controller/password.js
+++ server_Smeta/Controller/password.js
@@ -12,7 +12,9 @@ async function sendAuthCode(req, res) {
   const { email } = req.body;
   const code = generateAuthCode();
   const usercode = await user.updateOne({email:req.body.email},{code:code})
-  if(usercode.modifiedCount=== 1){
+  const usernamecode = await user.findOne({username:req.body.username})
+
+  if(usercode.modifiedCount=== 1 && usernamecode){
     const transporter = nodemailer.createTransport({
         service: 'Gmail', 
         auth: {
@@ -36,7 +38,8 @@ async function sendAuthCode(req, res) {
         }
       });
   }else{
-    res.status(401).json('Email không tồn tại')
+    let message = usercode.modifiedCount=== 1 ? 'Username không chính xác' : 'Email không tồn tại'
+    res.status(401).json(message)
   }  
 }
 
@@ -45,13 +48,13 @@ async function passwordretrieval(req, res) {
     try {
       const users = await user.findOne({ email:req.body.email, code: req.body.code }); 
       if (!users) {
-        return res.status(404).json({ error: 'Không tìm thấy thông tin người dùng' });
+        return res.status(404).json({ error: 'Mã xác thực không chính xác' });
       }
       const data = await user.updateOne({email:req.body.email},{code:null})
       const bta = await user.findOne({email:req.body.email})
       const pass = bta.password
-      const passwordMatch = await bcrypt.compare(password,pass);
-      res.status(200).json({ message: 'Thành công',passwordMatch });
+      // const passwordMatch = await bcrypt.compare(password,pass);
+      res.status(200).json({ message: 'Thành công',pass });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Lỗi khi truy vấn cơ sở dữ liệu' });
